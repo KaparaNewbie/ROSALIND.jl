2f93fbca9432de7c2b63e19cff7e424cc3523258
kobish <shapirakobi@gmail.com>
2021-12-27 14:25:34 +0200

    solved IPRB

--- IPRB.jl
+++ IPRB.jl
@@ -13,44 +13,45 @@ organisms can mate.
 
 # Example
 ```julia-repl
-julia> dominanceprobabilty(2, 2, 2)
+julia> dominanceprob(2, 2, 2)
 0.78333
 ```
 """
 function dominanceprob(k::Int, m::Int, n::Int)
+    
     total = k + m + n
-    
-    domhomo1st = k / total
-    het1st = m / total
-    rechomo1st = n / total
 
-    domhomo2nd = k / (total - 1)
-    het2nd = m / (total - 1)
-    rechomo2nd = n / (total - 1)
+    # we need to calc the probability to have at least one dominant allele
+    # we can do this by the complementary prob: to have two recessive alleles
+    # (1) define the possible matings, and their probabilities
+    # (2) define the probabilities to have two recessive alleles for these matings
+    # (3) multiply (1) and (2) and sum
+    # (4) return 1 - (3)
 
-    domhomo1stand2d = (k / total) * ((k - 1) / (total - 1))
-    het1stand2d = (m / total) * ((m - 1) / (total - 1))
-    rechomo1stand2d = (n / total) * ((n - 1) / (total - 1))
+    # (A) possible matings
+    het1het2 = (m / total) * ((m-1) / (total - 1))
+    het1homrec2 = (m / total) * (n / (total - 1))
+    homrec1het2 = (n / total) * (m / (total - 1))
+    homrec1homrec2 = (n / total) * ((n - 1) / (total - 1))
+
+    # (B) two recessive gamets (one from each mate in a possible mating)
+    twohets = 1 / 4
+    onehetonehomrec = 1 / 2
+    twohomrec = 1
+
+    # (C) sum A & B
+    homrecoffspring = (het1het2 * twohets) + 
+                      ((het1homrec2 + homrec1het2) * onehetonehomrec) + 
+                      (homrec1homrec2 * twohomrec)
+
+    # (D) return the complementary of C
+    domoffspring = 1 - homrecoffspring
+
+    return domoffspring
 
 end
 
 
+round(dominanceprob(2, 2, 2), digits=5)
 
-# https://docs.julialang.org/en/v1/manual/types/#Parametric-Types
-# https://docs.julialang.org/en/v1/manual/constructors/#Parametric-Constructors
-
-
-
-struct Genotype 
-    dominantallelprob
-end
-
-DominantHomozygous
-
-struct DominantHomozygous <: Genotype end
-struct Heterozygous end
-struct RecessiveHomozygous end
-
-# function dominantallelprob(a::DominantHomozygous, b::Heterozygous)
-    
-# end
+round(dominanceprob(23, 22, 19), digits=5)
